e045669d613ba322fb1ed15446c5d839b2440a55
cks1031 <cks1031@naver.com>
2024-07-25 18:18:19 +0900

    html5 - 태크, 요소, 속성, 문서구조

--- main.py
+++ main.py
@@ -6,15 +6,21 @@ from app.routes.sungjuk import sungjuk_router
 from app.routes.css import css_router
 from app.routes.html import html_router
 from app.routes.jscript import jscript_router
+from app.routes.emp import emp_router
 
 app = FastAPI()
 templates = Jinja2Templates(directory="views/templates") # jinja2 설정
 
 # 외부 라우트 설정
 app.include_router(sungjuk_router, prefix='/sungjuk')
+
 app.include_router(html_router, prefix='/html')
+
 app.include_router(css_router, prefix='/css')
+
 app.include_router(jscript_router, prefix='/js')
+
+app.include_router(emp_router, prefix='/emp')
 
 # index 라우트
 @app.get("/", response_class=HTMLResponse)
