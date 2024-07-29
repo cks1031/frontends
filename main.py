072e7c2a318f5571f680b14b30d8866b956cd1ba
cks1031 <cks1031@naver.com>
2024-07-29 10:00:57 +0900

    static 경로 설정

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from starlette.responses import HTMLResponse
+from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
 from app.routes.sungjuk import sungjuk_router
@@ -10,6 +11,7 @@ from app.routes.emp import emp_router
 
 app = FastAPI()
 templates = Jinja2Templates(directory="views/templates") # jinja2 설정
+app.mount('/static', StaticFiles(directory='views/static'), name='static')
 
 # 외부 라우트 설정
 app.include_router(sungjuk_router, prefix='/sungjuk')
@@ -20,7 +22,6 @@ app.include_router(css_router, prefix='/css')
 
 app.include_router(jscript_router, prefix='/js')
 
-# 사원등록프로그램
 app.include_router(emp_router, prefix='/emp')
 
 # index 라우트
