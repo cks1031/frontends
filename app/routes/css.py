13ff265c860134c6595c5be8be8bf01c99c7eac1
cks1031 <cks1031@naver.com>
2024-08-01 17:13:12 +0900

    css3 - 레이아웃 : float

--- app/routes/css.py
+++ app/routes/css.py
@@ -38,3 +38,15 @@ async def position(req: Request):
 @css_router.get('/background')
 async def background(req: Request):
     return templates.TemplateResponse('css/07background.html', {'request': req})
+
+@css_router.get('/table')
+async def table(req: Request):
+    return templates.TemplateResponse('css/08table.html', {'request': req})
+
+@css_router.get('/float')
+async def float(req: Request):
+    return templates.TemplateResponse('css/09float.html', {'request': req})
+
+@css_router.get('/flex')
+async def flex(req: Request):
+    return templates.TemplateResponse('css/10flex.html', {'request': req})
