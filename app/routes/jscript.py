237a3ce7bef27fdd5213fb38591236fe013ff6c1
cks1031 <cks1031@naver.com>
2024-08-08 13:38:40 +0900

    예외처리 1

--- app/routes/jscript.py
+++ app/routes/jscript.py
@@ -47,3 +47,15 @@ async def function(req: Request):
 @jscript_router.get('/callback')
 async def callback(req: Request):
     return templates.TemplateResponse('js/09callback.html', {'request': req})
+
+@jscript_router.get('/except')
+async def js_except(req: Request):
+    return templates.TemplateResponse('js/10except.html', {'request': req})
+
+@jscript_router.get('/bom')
+async def bom(req: Request):
+    return templates.TemplateResponse('js/11bom.html', {'request': req})
+
+@jscript_router.get('/dom')
+async def dom(req: Request):
+    return templates.TemplateResponse('js/12dom.html', {'request': req})
