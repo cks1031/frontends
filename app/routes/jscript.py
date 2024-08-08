bed53d1cd6fc032323c10622b78a3e2bf9d96cb5
cks1031 <cks1031@naver.com>
2024-08-08 11:55:26 +0900

    콜백함수, 내장함수

--- app/routes/jscript.py
+++ app/routes/jscript.py
@@ -43,3 +43,7 @@ async def loop_while(req: Request):
 @jscript_router.get('/function')
 async def function(req: Request):
     return templates.TemplateResponse('js/08function.html', {'request': req})
+
+@jscript_router.get('/callback')
+async def callback(req: Request):
+    return templates.TemplateResponse('js/09callback.html', {'request': req})
